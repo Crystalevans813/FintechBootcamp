c33f77d7da2b8e75faa3ad2dcb324e467867756c
Ms. Crystal Evans <106713686+Crystalevans813@users.noreply.github.com>
2022-06-20 23:12:31 -0400

    list 01
    6.21.22

---
+++ lists-01.py
@@ -0,0 +1,23 @@
+# Create a list, `list_1`,  with `0`, `1`, `2`, `3` as values.
+list_1 = [0,1,2,3]
+print(list_1)
+
+# Create a list, `list_2` with `4`, `5`, `6`, `7` as values.
+list_2 = [4,5,6,7]
+print(list_2)
+
+# Create a list, `list_3` with `8`, `9`, `10`, `11` as values.
+list_3 = [ 8,9,10,11]
+print(list_3)
+
+# Print the 3rd index of `list_1`.
+list_1 [2]
+print(list_1[2])
+
+# Print the 1st index of `list_2`.
+list_2 [0]
+print(list_2[0])
+
+# Print the 2nd index of `list_3`.
+list_3 [1]
+print(list_3[1])
