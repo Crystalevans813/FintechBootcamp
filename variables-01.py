f815ba4f1e8a51650c69f25f680dbe48c53a5f59
Ms. Crystal Evans <106713686+Crystalevans813@users.noreply.github.com>
2022-06-20 22:40:07 -0400

    variables-01.py

---
+++ variables-01.py
@@ -0,0 +1,67 @@
+# Variables
+
+# This is a comment - any code commented out is not run -
+# meaning that any code or text commented out is ignored.
+
+"""
+This is a multi-line comment.
+You can comment out many lines at once.
+For the most part you will see single line comments
+As in single line comments, any code contained within multi-line comments is not run -
+meaning that any code commented out is ignored.
+"""
+
+# Topic: Strings
+
+# Create a variable named `subject` with no value (None).
+subject = None
+print(subject)
+# Assign a value of "Programmers to the variable `subject`.
+programmers = subject 
+print ("The value of a is: " + str(subject))
+
+# Create a variable, `first_name`, and assign it a value of an empty string.
+first_name = ""
+print(first_name)
+
+# Assign a value of "Ada" to the variable `first_name`.
+first_name = "Ada"
+print(first_name)
+
+# Create a variable, `last_name`, and assign it a value of a string, "Lovelace".
+last_name = "Lovelace"
+print(last_name)
+
+# Topic: Integers
+
+# Create a variable, `birth_year`, and assign it with an integer of 1815.
+# print num in decimal and binary format
+birth_year = 1815 
+print ("num (decimal) : ", 1815 )
+print ("num (binary ) : ", bin (1815 ))
+
+
+# Create a variable, death_year, and assign it with an integer of 1852.
+birth_year = 1852
+print ("num (decimal) : ", 1852 )
+print ("num (binary ) : ", bin (1852))
+
+# Create a variable, age_at_passing, and assign it a value of death_year minus birth_year.
+age_at_passing = 1852-1815
+print(age_at_passing)
+
+#Topic: Print
+
+# Print: "First Name: " and `first_name`.
+
+
+# Print: "Last Name: " and `last_name`.
+# YOUR CODE HERE!
+
+
+#Topic: Concat Values
+
+# Create and print a variable, `statement_one`, by assigning it a value of a string:
+# "Programmers: Ada Lovelace was born in 1815, and she lived to be 37 years old."
+statement_one = "Programmers: Ada Lovelace was born in 1815, and she lived to be 37 years old."
+print(statement_one)
